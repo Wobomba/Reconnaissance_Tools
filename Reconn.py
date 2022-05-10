89c4486e1d9aab5a14fe1e648a7d7db14e34a96f
Wobomba <iwobomba999@gmail.com>
2022-05-10 13:32:46 +0300

    Initial

--- Reconn.py
+++ Reconn.py
@@ -12,9 +12,115 @@ def obtain_ip():
         stp.close()
     return IP
 
+#port scanning function
+def portScans():
+    portNum = int(input("Port Number: "))
+    option = ["1. Port selection", "2. Port Scan Types"]
+    print("\n".join(option))
+    try:
+        pOption = int(input("Option: "))
+        match pOption:
+            case 1:
+                portScan = ["1. Specific port", "2. Scan common ports", "3. Scan all 65535 ports", "4. Scan a range of ports [1-100]"]
+                print("\n".join(portScan))
+                pScan = int(input("Option: "))
+                match pScan:
+                    case 1:
+                        os.system("sudo nmap -p "+portNum+ obtain_ip())
+                    case 2:
+                        os.system("sudo nmap -F "+obtain_ip())
+                    case 3:
+                        os.system("sudo nmap -p- "+obtain_ip())
+                    case 4:
+                        os.system("sudo nmap -p 1-100 "+obtain_ip())
+                    case _:
+                        print("Option does not exist")
+                        portScans()
+            case 2:
+                Soption = ["1. TCP connect", "2. TCP SYN", "3. UDP ports", "4. Selected ports[IGNORE DISCOVERY]]"]
+                print("\n".join(Soption))
+                Scoption = int(input("Option: "))
+                match Scoption:
+                    case 1:
+                        os.system("sudo nmap -sT "+obtain_ip())
+                    case 2:
+                        os.system("sudo nmap -sS "+obtain_ip())
+                    case 3:
+                        os.system("sudo nmap -sU -p 123,161,162 "+obtain_ip())
+                    case 4:
+                        os.system("sudo nmap -Pn -F "+obtain_ip())
+                    case _:
+                        print("Option does not exist")
+                        portScans()
+    except:
+        print("Option does not exist")
+        portScans()
+
+#service and os detection
+def SoD():
+    option = ["1. OS Fingerprinting", "2. Standard Service ", "3. Aggressive Service", "4. Lighter Banner Grabbing"]
+    print("\n".join(option))
+    try:
+        Soption = int(input("Option: "))
+        match Soption:
+            case 1:
+                os.system("sudo nmap -A "+obtain_ip())
+            case 2:
+                os.system("sudo nmap -sV "+obtain_ip())
+            case 3: 
+                os.system("sudo nmap -sV --version-intensity 5 "+obtain_ip())
+            case 4:
+                os.system("sudo nmap -sV --version-intensity 0 "+obtain_ip())
+            case _:
+                print("Option does not exist")
+                SoD()
+    except:
+        print("Option does not exist")
+        SoD()
+#user output option
+def Uoo():
+    option = ["1. To file", "2. To XML", "3. To GREP", "4. All formats"]
+    print("\n".join(option))
+    try:
+        Uoption = int(input("Option: "))
+        match Uoption:
+            case 1:
+                os.system("sudo nmap -oN outputfile.txt "+obtain_ip())
+            case 2:
+                os.system("sudo nmap -oX outputfile.xml "+obtain_ip())
+            case 3:
+                os.system("sudo nmap -oG outputfile.txt "+obtain_ip())
+            case 4:
+                os.system("sudo nmap -oA outputfile.txt "+obtain_ip())
+            case _:
+                print("Option does not exist")
+                Uoo()
+    except:
+        print("Option does not exist")
+        Uoo()
+
+#advanced scanning using NSE scripts
+def AsU():
+    scriptType = ["1. Default safe scripts","2. Help", "3. Specific NSE script", "4. Set of scripts"]
+    print("\n".join(scriptType))
+    try:
+        scriptOption = int(input("Option: "))
+        match scriptOption:
+            case 1:
+                os.system("sudo -sV -sC "+obtain_ip())
+            case 2:
+                os.system("sudo --script-help=ssl-heartbleed")
+            case 3:
+                os.system("sudo nmap -sV -p 443 -script=ssl-hearbleed.nse "+obtain_ip())
+            case 4:
+                os.system("sudo nmap -sV --script=smb* "+obtain_ip())
+    except:
+        print("Option does not exist")
+        AsU()
+
 #function for active scanning
 def activeScan():
-    option = ["1. Local Network", "2. Port Scanning", "3. Service and OS Detection", "4. Packet Tracing", "5. Remote Network", "6. Online Targets"]
+    option = ["1. Local Network", "2. Port Scanning", "3. Service and OS Detection", "4. Packet Tracing", "5. Remote Network", "6. DDOS Reflection UDP Services", "Advanced Scanning with NSE"]
     print("\n".join(option))
     try:
         attackType = int(input("Select option: "))
@@ -38,21 +144,18 @@ def activeScan():
                 os.system("sudo nmap -F "+remoteNet)
                 exit()
             case 6:
-                portNum = int(input("Port Number: "))
-                os.system("sudo nmap -PR "+portNum)
-                spcprt0 = "sudo nmap -p"+str(portNum)+" -oG - "+obtain_ip()+" -D"
-                spcprt1 = "sudo nmap -p "+str(portNum)+" -oG - "+obtain_ip()+" -D| awk "+"/"+str(portNum)+"\/open/{print $2}"+">> ports.txt"
-                spcprt2 = "sudo nmap -o -iL ports.txt"
-                print(f'[OS ID:] {spcprt2}')
-                scn = subprocess.run([spcprt0,spcprt1,spcprt2])
-                exit()
+                CID = str(input("Enter IP with CIDR: "))
+                os.system("sudo nmap -sU -A -PN -n -pU:19,53,123,161 -script=ntp-monlist, dns-recursion,snmp-sysdescr "+CID)
+                
+            case 7:
+                AsU()
+
             case _:
                 print("Option does not exist")
                 activeScan()
     except:
         print("Option does not exist")
         activeScan()
-
 
 #function for passive scanning
 def passiveScan():
@@ -74,6 +177,7 @@ def passiveScan():
         print("Option does not exist")
         passiveScan()
 
+
 #initial function
 def index():
     scanType = ['1. Active Scanning', '2. Passive Scanning']
@@ -92,80 +196,14 @@ def index():
         print("Option does not exist")
         index()
 
-#port scanning function
-def portScans():
-    portNum = int(input("Port Number: "))
-    option = ["1. Port selection", "2. Port Scan Types"]
-    print("\n".join(option))
-    pOption = int(input("Option: "))
-    match pOption:
-        case 1:
-            portScan = ["1. Specific port", "2. Scan common ports", "3. Scan all 65535 ports", "4. Scan a range of ports [1-100]"]
-            print("\n".join(portScan))
-            pScan = int(input("Option: "))
-            match pScan:
-                case 1:
-                    os.system("sudo nmap -p "+portNum+ obtain_ip())
-                case 2:
-                    os.system("sudo nmap -F "+obtain_ip())
-                case 3:
-                    os.system("sudo nmap -p- "+obtain_ip())
-                case 4:
-                    os.system("sudo nmap -p 1-100 "+obtain_ip())
-                case _:
-                    print("Option does not exist")
-                    portScans()
-        case 2:
-            Soption = ["1. TCP connect", "2. TCP SYN", "3. UDP ports", "4. Selected ports[IGNORE DISCOVERY]]"]
-            print("\n".join(Soption))
-            Scoption = int(input("Option: "))
-            match Scoption:
-                case 1:
-                    os.system("sudo nmap -sT "+obtain_ip())
-                case 2:
-                    os.system("sudo nmap -sS "+obtain_ip())
-                case 3:
-                    os.system("sudo nmap -sU -p 123,161,162 "+obtain_ip())
-                case 4:
-                    os.system("sudo nmap -Pn -F "+obtain_ip())
-                case _:
-                    print("Option does not exist")
-                    portScans()
 
-#service and os detection
-def SoD():
-    option = ["1. OS Fingerprinting", "2. Standard Service ", "3. Aggressive Service", "4. Lighter Banner Grabbing"]
-    print("\n".join(option))
-    Soption = int(input("Option: "))
-    match Soption:
-        case 1:
-            os.system("sudo nmap -A "+obtain_ip())
-        case 2:
-            os.system("sudo nmap -sV "+obtain_ip())
-        case 3: 
-            os.system("sudo nmap -sV --version-intensity 5 "+obtain_ip())
-        case 4:
-            os.system("sudo nmap -sV --version-intensity 0 "+obtain_ip())
-        case _:
-            print("Option does not exist")
-            SoD()
-#user output option
-def Uoo():
-    option = ["1. To file", "2. To XML", "3. To GREP", "4. All formats"]
-    print("\n".join(option))
-    Uoption = int(input("Option: "))
-    match Uoption:
-        case 1:
-            os.system("sudo nmap -oN outputfile.txt "+obtain_ip())
-        case 2:
-            os.system("sudo nmap -oX outputfile.xml "+obtain_ip())
-        case 3:
-            os.system("sudo nmap -oG outputfile.txt "+obtain_ip())
-        case 4:
-            os.system("sudo nmap -oA outputfile.txt "+obtain_ip())
-        case _:
-            print("Option does not exist")
-            Uoo()
+
+
+
+
+
+
+
 
         
 
